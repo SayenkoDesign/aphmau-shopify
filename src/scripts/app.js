535511b8be70263573e97c1d084dc38d88a6e59e
Robert M. Parker <rparker@yamiko.ninja>
2016-10-12 04:11:05 -0700

    finish first draft of pahmau

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,26 @@
 jQuery(function() {
     jQuery(document).foundation();
-    jQuery('.fancybox').fancybox();
+    jQuery('.fancybox').fancybox({
+        openEffect  : 'none',
+        closeEffect : 'none',
+        helpers : {
+            overlay: {
+                locked: false
+            }
+        },
+        afterLoad: function(current, previous) {
+            jQuery('.slick').slick('slickGoTo', current.index-1);
+            console.log('moved to ' + (current.index-1));
+        }
+    });
+    jQuery('.slick').slick({
+        arrows: true,
+        dots: false,
+        lazyLoad: 'ondemand',
+        autoplay: false
+    });
+    jQuery('.slick-nav').click(function(){
+        var slide = jQuery(this).parent().index();
+        jQuery('.slick').slick('slickGoTo', slide);
+    })
 });
